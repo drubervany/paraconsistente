bcf5c32e6deb76419ea228ef549ce856377ea882
Danilo Rubervany <drubervany@gmail.com>
2017-10-04 21:47:25 -0300

    Abstração de id e nome

--- src/main/java/br/com/paraconsistente/models/CFPS.java
+++ src/main/java/br/com/paraconsistente/models/CFPS.java
@@ -1,8 +1,6 @@
 package br.com.paraconsistente.models;
 
-public class CFPS {
-
-    private Especialista especialista;
+public class CFPS extends AbstractIdName {
 
     private Projeto projeto;
 
@@ -10,15 +8,11 @@ public class CFPS {
 
     private Medicao medicao;
 
-    public CFPS(Especialista especialista, Projeto projeto, Integer numeroPontos, Medicao medicao) {
-        this.especialista = especialista;
+    public CFPS(Integer id, String nome, Projeto projeto, Integer numeroPontos, Medicao medicao) {
+        super(id, nome);
         this.projeto = projeto;
         this.numeroPontos = numeroPontos;
         this.medicao = medicao;
-    }
-
-    public Especialista getEspecialista() {
-        return especialista;
     }
 
     public Projeto getProjeto() {
--- src/main/java/br/com/paraconsistente/models/Cliente.java
+++ src/main/java/br/com/paraconsistente/models/Cliente.java
@@ -1,23 +1,9 @@
 package br.com.paraconsistente.models;
 
-public class Cliente {
-
-    private Integer id;
-
-    private String nome;
+public class Cliente extends AbstractIdName {
 
     public Cliente(Integer id, String nome) {
-        super();
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
+        super(id, nome);
     }
 
 }
