1191f7cfe85d2a65689ce429f4865bb25bf587ec
Danilo Rubervany <drubervany@gmail.com>
2017-10-04 21:48:20 -0300

    Abstração de id e nome

--- src/main/java/br/com/paraconsistente/models/Cargo.java
+++ src/main/java/br/com/paraconsistente/models/Cargo.java
@@ -1,23 +1,9 @@
 package br.com.paraconsistente.models;
 
-public class Cargo {
-
-    private final Integer id;
-
-    private final String nome;
+public class Cargo extends AbstractIdName {
 
     public Cargo(Integer id, String nome) {
-        super();
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
+        super(id, nome);
     }
 
 }
